80aee8c9b21c1e9a92de052c24ba26aa59b345bc
P-WJ <pwj1103@gmail.com>
2025-07-17 20:33:09 +0900

    #19 feat: 유저 영화 정보 구현(wishlist, recent, favorite, dislike)

---
+++ cinever-FE/src/api/user.js
@@ -0,0 +1,53 @@
+import api from "./index";
+
+const userApi = "/api/user";
+
+export const getUserInfo = async (id) => {
+  try {
+    const response = await api.get(`${userApi}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 실패:", error);
+    throw error;
+  }
+};
+
+export const getUserWishlistInfo = async (id) => {
+  try {
+    const response = await api.get(`${userApi}/${id}/wishlist`);
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 실패:", error);
+    throw error;
+  }
+};
+
+export const getUserRecentInfo = async (id) => {
+  try {
+    const response = await api.get(`${userApi}/${id}/recent-movie`);
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 실패:", error);
+    throw error;
+  }
+};
+
+export const getUserFavoriteInfo = async (id) => {
+  try {
+    const response = await api.get(`${userApi}/${id}/favorite-movie`);
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 실패:", error);
+    throw error;
+  }
+};
+
+export const getUserDislikeInfo = async (id) => {
+  try {
+    const response = await api.get(`${userApi}/${id}/dislike-movie`);
+    return response.data;
+  } catch (error) {
+    console.error("API 호출 실패:", error);
+    throw error;
+  }
+};
