ff014d09d2d3a137d9aef234103ea8e92229979b
OhChangEun <dwc07109@gmail.com>
2025-07-17 15:06:14 +0900

    #1 feat: 상단 헤더 슬라이더바 UI 추가

---
+++ cinever-FE/src/components/common/BaseSlideButton.vue
@@ -0,0 +1,71 @@
+<!-- components/common/SlideButton.vue -->
+<script setup>
+import { computed, ref } from "vue";
+
+const props = defineProps({
+  label: String, // 나타날 라벨
+  icon: [Object, String], // 컴포넌트 또는 이미지 URL
+  color: { type: String, default: "white" }, // 아이콘 색상
+  hoverBg: { type: String, default: "bg-gray-800" }, // hover 배경색
+  borderColor: { type: String, default: "border border-white" }, // 아이콘 테두리 색상
+  onClick: Function, // 클릭 핸들러
+  isSearch: { type: Boolean, default: false }, // 검색 모드 여부
+  modelValue: String, // 검색 input의 값
+});
+
+const isSliderOpen = ref(false);
+
+const handleClick = () => {
+  if (props.onClick) props.onClick();
+};
+
+const isImage = computed(() => {
+  typeof props.icon === "string";
+});
+
+const updateInput = (e) => {
+  emit("update:modelValue", e.target.value);
+};
+</script>
+
+<template>
+  <div
+    class="relative select-none"
+    @mouseenter="isSliderOpen = true"
+    @mouseleave="isSliderOpen = false"
+    @click="handleClick"
+  >
+    <div
+      class="flex justify-center items-center px-1 hover:pr-3 py-1 rounded-full transition cursor-pointer"
+      :class="hoverBg"
+    >
+      <!-- 아이콘 영역 -->
+      <div
+        class="w-8 h-8 rounded-full overflow-hidden flex justify-center items-center"
+        :class="borderColor"
+      >
+        <!-- 1) 유저 이미지  -->
+        <img
+          v-if="isImage"
+          :src="icon"
+          class="w-5 h-5 object-cover"
+          alt="profile"
+        />
+        <!-- 2) 아이콘  -->
+        <component v-else :is="icon" class="w-5 h-5" :class="`text-${color}`" />
+      </div>
+
+      <!-- 라벨 -->
+      <span
+        :class="[
+          ' text-sm font-medium whitespace-nowrap transition-all duration-300 overflow-hidden',
+          isSliderOpen
+            ? 'ml-2 max-w-[200px] opacity-100 text-white'
+            : 'ml-0 max-w-0 opacity-0',
+        ]"
+      >
+        {{ isSliderOpen ? label : "" }}
+      </span>
+    </div>
+  </div>
+</template>
